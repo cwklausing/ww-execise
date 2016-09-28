27aa0ebf6ab936dea2aa1d6133c61b17f9034a18
cwklausing <cwklausing@gmail.com>
2016-09-28 16:09:05 -0500

    Values slider complete

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,7 +1,35 @@
 $(function(){
-  console.log("hello!");
+  /*Create function to grab four people from database.
+  Should also track position in the list so we don't
+  get repeat people.
 
-  //Mobile menu click
+  if there aren't four left, just take as many as there are.
+  */
+  function getEmployees () {
+    $.ajax({
+      url: '/data/Employees.json',
+      method: 'GET'
+    }).done(function(data) {
+      console.log(data);
+    })
+  }
+
+  // Team values click handler
+  $('.value-dropdown').on('click', function() {
+    if($(this).data("open")) {
+      console.log("closing");
+      $(this).siblings().slideToggle();
+      $(this).data("open", false);
+    }
+    else {
+      console.log("opening");
+      $('.value-text').slideUp();
+      $(this).data("open", true);
+      $(this).siblings().slideToggle();
+    }
+  })
+
+  // Mobile menu click
   var menuOpen = false;
   $('.menu-button').on('click', function() {
   	// Set offset of menu based on device width
